720631a507d90c9e4eba82c249dcb19944b7710c
thomas <thomasnuernberg@icloud.com>
2025-10-29 14:27:33 +0100

    aufräumen

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,53 +2,34 @@ package h3;
 
 public class H3_main {
 
-    static int i = 2;
-    static int j = 3;
-    static int k = 7;
-
-
-
-    static boolean a1(){
-        if(i > j){
-            return true;
-        }
-        return false;
-    }
-    static boolean a2(){
-        if(i > 200){
-            return true;
-        }
-        return false;
-    }
-    static boolean a3(){
-        if(j > 100){
-            return true;
-        }
-        a1();
-        return false;
-    }
-
-
-
     public static void main(String[] args){
+
+        int i = 210;
+        int j = 102;
+        int k = 7;
+
+        boolean anweisung1 = i > j;
+        boolean anweisung2 = i > 200;
+        boolean anweisung3 = j > 100;
 
         k = -10;
 
-        if(a1() && !a2() && !a3() ){
+        if(anweisung1 && !anweisung2 && !anweisung3 ){
             k = 1;
         }
-        if(a1() && a2() && !a3() ){
+        if(anweisung1 && anweisung2 && !anweisung3 ){
             k = 2;
         }
 
-        if(a1() && a2() && a3() ){
+        if(anweisung1 && anweisung2 && anweisung3 ){
             k = 3;
         }
 
-        if(!a1() && !a2() && !a3() ){
+        if(!anweisung1 && !anweisung2 && !anweisung3 ){
             k = 4;
         }
         System.out.println(k);
+        
 
     }
 
